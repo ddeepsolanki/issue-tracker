20b29c2821168684f2aa29c26657fd47bef77be5
Deep Solanki <workitoutdeep@gmail.com>
2025-05-06 20:26:20 +0530

    Delete an issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,6 +1,10 @@
+"use client";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+  const router = useRouter();
   return (
     <AlertDialog.Root>
       <AlertDialog.Trigger>
@@ -8,14 +12,28 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
       </AlertDialog.Trigger>
       <AlertDialog.Content>
         <AlertDialog.Title>Confirm Delete</AlertDialog.Title>
-        <AlertDialog.Description>Are you sure you want to delete this issue? This action cannot be undone.</AlertDialog.Description>
-        <Flex gap='4' mt='3'>
-            <AlertDialog.Cancel>
-                <Button variant='soft' color="gray">Cancle</Button>
-            </AlertDialog.Cancel>
-            <AlertDialog.Action>
-                <Button color="red">Delete Issue</Button>
-            </AlertDialog.Action>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action cannot be
+          undone.
+        </AlertDialog.Description>
+        <Flex gap="4" mt="3">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancle
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button
+              color="red"
+              onClick={async () => {
+                await axios.delete("/api/issues/" + issueId);
+                router.push("/issues");
+                router.refresh();
+              }}
+            >
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
         </Flex>
       </AlertDialog.Content>
     </AlertDialog.Root>
